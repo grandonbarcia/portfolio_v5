fb3464be08b8c6b2d69f3f3d7d473a7c4a4ebf11
Brandon Garcia <grandonleebarcia@gmail.com>
2025-05-06 17:27:40 -0700

    edit styles

--- app/components/Piano.tsx
+++ app/components/Piano.tsx
@@ -18,43 +18,43 @@ export default function Piano() {
         <div className="relative h-3/4 w-full">
           <Button
             onClick={() => play('C5')}
-            className="h-full bg-white dark:bg-black dark:text-white hover:bg-slate-200  border shadow text-black items-end"
+            className="h-full bg-white  hover:bg-slate-200  border shadow text-black items-end cursor-pointer"
           >
             C
           </Button>
           <Button
             onClick={() => play('D5')}
-            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end"
+            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end cursor-pointer"
           >
             D
           </Button>
           <Button
             onClick={() => play('E5')}
-            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end"
+            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end cursor-pointer"
           >
             E
           </Button>
           <Button
             onClick={() => play('F5')}
-            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end"
+            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end cursor-pointer"
           >
             F
           </Button>
           <Button
             onClick={() => play('G5')}
-            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end"
+            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end cursor-pointer"
           >
             G
           </Button>
           <Button
             onClick={() => play('A5')}
-            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end"
+            className="h-full bg-white hover:bg-slate-200  border shadow text-black items-end cursor-pointer"
           >
             A
           </Button>
           <Button
             onClick={() => play('B5')}
-            className="h-full bg-white hover:bg-slate-200 border shadow text-black items-end"
+            className="h-full bg-white hover:bg-slate-200 border shadow text-black items-end cursor-pointer"
           >
             B
           </Button>
@@ -62,24 +62,33 @@ export default function Piano() {
         <div className="absolute h-1/2 w-full">
           <Button
             onClick={() => play('Db5')}
-            className="h-full ml-5 items-end hover:bg-zinc-800"
+            className="h-full ml-5 items-end dark:bg-black dark:text-white dark:hover:bg-slate-800 cursor-pointer"
           >
             <p className="">D</p>
             <p className="absolute ml-4">b</p>
           </Button>
-          <Button onClick={() => play('Eb5')} className="h-full ml-1 items-end">
+          <Button
+            onClick={() => play('Eb5')}
+            className="h-full ml-1 items-end dark:bg-black dark:text-white dark:hover:bg-slate-800 cursor-pointer"
+          >
             E<p className="absolute ml-4">b</p>
           </Button>
           <Button
             onClick={() => play('Gb5')}
-            className="h-full ml-10.5 items-end"
+            className="h-full ml-10.5 items-end dark:bg-black dark:text-white dark:hover:bg-slate-800 cursor-pointer"
           >
             G<p className="absolute ml-4">b</p>
           </Button>
-          <Button onClick={() => play('Ab5')} className="h-full ml-1 items-end">
+          <Button
+            onClick={() => play('Ab5')}
+            className="h-full ml-1 items-end dark:bg-black dark:text-white dark:hover:bg-slate-800 cursor-pointer"
+          >
             A<p className="absolute ml-4">b</p>
           </Button>
-          <Button onClick={() => play('Bb5')} className="h-full ml-1 items-end">
+          <Button
+            onClick={() => play('Bb5')}
+            className="h-full ml-1 items-end dark:bg-black dark:text-white dark:hover:bg-slate-800 cursor-pointer"
+          >
             B<p className="absolute ml-4">b</p>
           </Button>
 
