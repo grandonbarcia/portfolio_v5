90b78a160d4a5eb08f2bb2c5f321ead6f259bc39
Brandon Garcia <grandonleebarcia@gmail.com>
2025-05-06 17:25:58 -0700

    added animations

--- app/page.tsx
+++ app/page.tsx
@@ -1,36 +1,31 @@
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
 import { ModeToggle } from './components/ModeToggle';
-import { Badge } from '@/components/ui/badge';
 import { FaFileDownload } from 'react-icons/fa';
 import { FaLinkedin } from 'react-icons/fa';
 import { FaGithub } from 'react-icons/fa';
 import { IoIosMail } from 'react-icons/io';
-// import { FaHtml5 } from 'react-icons/fa';
-// import { SiCsswizardry } from 'react-icons/si';
 import { SiTypescript } from 'react-icons/si';
 import { FaReact } from 'react-icons/fa';
 import { RiNextjsFill } from 'react-icons/ri';
 import { RiTailwindCssFill } from 'react-icons/ri';
 import { PiCopyrightFill } from 'react-icons/pi';
-import { PiMonitor } from 'react-icons/pi';
-import { FiGithub } from 'react-icons/fi';
 import Headshot from '@/assets/headshot.jpg';
 import WorkExperience from './components/WorkExperience';
 import Kanban from '@/assets/kanban.jpg';
 import Memory from '@/assets/memory.jpg';
-
+import MagneticIcon from './components/MagneticIcon';
+import Piano from './components/Piano';
+import ProjectCard from './components/ProjectCard';
 import {
   Card,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle,
 } from '@/components/ui/card';
-import Piano from './components/Piano';
 
 const SOCIALS_SIZE = 25;
-const TECH_SIZE = 25;
+const TECH_SIZE = 30;
 
 export default function Home() {
   return (
@@ -51,18 +46,56 @@ export default function Home() {
               31-year-old software developer from San Jose, CA
             </div>
             <div className="pb-4">
-              I like to build interactive front-end apps, play the piano, and
-              drink coffee
+              I’m all about crafting interactive front-end apps, jamming out on
+              my piano, and surviving on way too much coffee.
             </div>
-            <div className="font-semibold">Available for work </div>
+            <div className="flex items-center gap-2 font-semibold">
+              <span className="relative flex size-3">
+                <span className="absolute inline-flex h-full w-full animate-ping rounded-full bg-green-400 opacity-75"></span>
+                <span className="relative inline-flex size-3 rounded-full bg-green-500"></span>
+              </span>
+              <span className="underline">Available for work</span>
+            </div>
             <div className="flex items-center gap-8 mt-10 ">
-              <Button>
-                Resume <FaFileDownload />
-              </Button>
+              <a
+                href="/resume.pdf"
+                target="_blank"
+                rel="noopener"
+                className="cursor-pointer"
+              >
+                <Button className="group cursor-pointer">
+                  Resume
+                  <FaFileDownload className="group-hover:animate-bounce" />
+                </Button>
+              </a>
               <div className="flex gap-6">
-                <FaLinkedin size={SOCIALS_SIZE} />
-                <FaGithub size={SOCIALS_SIZE} />
-                <IoIosMail size={SOCIALS_SIZE} />
+                <MagneticIcon>
+                  <a
+                    href="https://www.linkedin.com/in/brandon-garcia-54708516b/"
+                    target="_blank"
+                  >
+                    <FaLinkedin
+                      size={SOCIALS_SIZE}
+                      className="hover:text-sky-600 "
+                    />
+                  </a>
+                </MagneticIcon>
+                <MagneticIcon>
+                  <a href="https://github.com/grandonbarcia" target="_blank">
+                    <FaGithub
+                      size={SOCIALS_SIZE}
+                      className="hover:text-green-600"
+                    />
+                  </a>
+                </MagneticIcon>
+                <MagneticIcon>
+                  <a href="mailto:grandonleebarcia@gmail.com">
+                    <IoIosMail
+                      size={SOCIALS_SIZE}
+                      className="hover:text-red-700"
+                    />
+                  </a>
+                </MagneticIcon>
               </div>
             </div>
           </div>
@@ -74,67 +107,93 @@ export default function Home() {
         </div>
       </div>
       <div className="relative my-6 grid  grid-cols-4 gap-2  ">
-        <div className="flex flex-col items-center justify-center border py-3 rounded-2xl">
-          <RiTailwindCssFill size={TECH_SIZE} />
-          <div className="mt-2 ">Tailwind CSS</div>
-        </div>
-        <div className="flex flex-col items-center justify-center border  rounded-2xl">
-          <SiTypescript size={TECH_SIZE} />
+        <Card className="group">
+          <CardHeader className="flex flex-col items-center justify-center">
+            <CardTitle>
+              <RiTailwindCssFill
+                size={TECH_SIZE}
+                className="transition delay-150 duration-300 ease-in-out group-hover:-translate-y-1  group-hover:scale-110  group-hover:text-cyan-500"
+              />
+            </CardTitle>
+            <CardDescription>Tailwind CSS</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="group">
+          <CardHeader className="flex flex-col items-center justify-center">
+            <CardTitle>
+              <SiTypescript
+                size={TECH_SIZE}
+                className="transition delay-150 duration-300 ease-in-out group-hover:-translate-y-1  group-hover:scale-110  group-hover:text-blue-500"
+              />
+            </CardTitle>
+            <CardDescription>TypeScript</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="group">
+          <CardHeader className="flex flex-col items-center justify-center">
+            <CardTitle>
+              <FaReact
+                size={TECH_SIZE}
+                className="transition delay-150 duration-300 ease-in-out group-hover:-translate-y-1  group-hover:scale-110  group-hover:text-blue-300"
+              />
+            </CardTitle>
+            <CardDescription>React.js</CardDescription>
+          </CardHeader>
+        </Card>
+        <Card className="group">
+          <CardHeader className="flex flex-col items-center justify-center">
+            <CardTitle>
+              <RiNextjsFill
+                size={TECH_SIZE}
+                className="transition delay-150 duration-300 ease-in-out group-hover:-translate-y-1  group-hover:scale-120  "
+              />
+            </CardTitle>
+            <CardDescription>Next.js</CardDescription>
+          </CardHeader>
+        </Card>
+
+        {/* <div className="group flex flex-col items-center justify-center border  rounded-2xl">
+          <SiTypescript
+            size={TECH_SIZE}
+            className="transition delay-150 duration-300 ease-in-out group-hover:-translate-y-1  group-hover:scale-110  group-hover:text-blue-500"
+          />
           <div className="mt-2">Typescript</div>
         </div>
-        <div className="flex flex-col items-center justify-center border rounded-2xl">
-          <FaReact size={TECH_SIZE} />
+        <div className="group flex flex-col items-center justify-center border rounded-2xl">
+          <FaReact
+            size={TECH_SIZE}
+            className="transition delay-150 duration-300 ease-in-out group-hover:-translate-y-1  group-hover:scale-110  group-hover:text-blue-300"
+          />
           <div className="mt-2">React.js</div>
         </div>
-        <div className="flex flex-col items-center justify-center border  rounded-2xl">
-          <RiNextjsFill size={TECH_SIZE} />
+        <div className="group flex flex-col items-center justify-center border  rounded-2xl">
+          <RiNextjsFill
+            size={TECH_SIZE}
+            className="transition delay-150 duration-300 ease-in-out group-hover:-translate-y-1  group-hover:scale-110  "
+          />
           <div className="mt-2">Next.js</div>
-        </div>
+        </div> */}
       </div>
-      <Piano />
+
+      {/* <Piano /> */}
       <WorkExperience />
 
-      <div>
-        <div className="text-3xl font-semibold mb-8">personal projects</div>
-        <div className="flex justify-between gap-4">
-          <Card className="w-1/2 shadow-md">
-            <CardHeader>
-              <Image src={Kanban} alt="Kanban" className="rounded" />
-              <CardTitle>Kanban Board</CardTitle>
-              <CardDescription>
-                Drag and drop your tasks to organize your work flow
-              </CardDescription>
-            </CardHeader>
-            <CardFooter className="gap-2">
-              <Badge variant="outline" className="cursor-pointer">
-                <FiGithub />
-                Source
-              </Badge>
-              <Badge variant="outline" className="cursor-pointer">
-                <PiMonitor /> Live
-              </Badge>
-            </CardFooter>
-          </Card>
-          <Card className="w-1/2 shadow-md">
-            <CardHeader>
-              <Image src={Memory} alt="Memory" className="rounded" />
-              <CardTitle>Memory Game</CardTitle>
-              <CardDescription>
-                Test your memory solo or with friends
-              </CardDescription>
-            </CardHeader>
-            <CardFooter className="gap-2">
-              <Badge variant="outline" className="cursor-pointer">
-                <FiGithub />
-                Source
-              </Badge>
-              <Badge variant="outline" className="cursor-pointer">
-                <PiMonitor /> Live
-              </Badge>
-            </CardFooter>
-          </Card>
-        </div>
-      </div>
+      <div className="text-3xl font-semibold mb-8">personal projects</div>
+
+      <ProjectCard
+        title={'Kanban Board'}
+        image={Kanban}
+        description={'Drag and drop your tasks to organize your work flow'}
+        github={'https://github.com/grandonbarcia/kanban-board.git'}
+        live={'https://merry-blini-eb0b20.netlify.app/'}
+      />
+      <ProjectCard
+        title={'Memory Game'}
+        image={Memory}
+        description={'Test your memory solo or with friends'}
+        github={'https://github.com/grandonbarcia/memory-game'}
+        live={'https://cheery-scone-e35758.netlify.app/'}
+      />
       <div className="border-t-2 pb-4">
         <div className="flex justify-between  pt-4">
           <div className="flex items-center">
@@ -142,9 +201,30 @@ export default function Home() {
             BG
           </div>
           <div className="flex gap-6">
-            <FaLinkedin size={SOCIALS_SIZE} />
-            <FaGithub size={SOCIALS_SIZE} />
-            <IoIosMail size={SOCIALS_SIZE} />
+            <MagneticIcon>
+              <a
+                href="https://www.linkedin.com/in/brandon-garcia-54708516b/"
+                target="_blank"
+              >
+                <FaLinkedin
+                  size={SOCIALS_SIZE}
+                  className="hover:text-sky-600"
+                />
+              </a>
+            </MagneticIcon>
+            <MagneticIcon>
+              <a href="https://github.com/grandonbarcia" target="_blank">
+                <FaGithub
+                  size={SOCIALS_SIZE}
+                  className="hover:text-green-600"
+                />
+              </a>
+            </MagneticIcon>
+            <MagneticIcon>
+              <a href="mailto:grandonleebarcia@gmail.com">
+                <IoIosMail size={SOCIALS_SIZE} className="hover:text-red-700" />
+              </a>
+            </MagneticIcon>
           </div>
         </div>
       </div>
